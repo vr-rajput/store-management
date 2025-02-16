1f09cb302f496ed0a85b0a7131eee24d8797f28a
vr-rajput <vikeshraj370@gmail.com>
2025-02-16 11:37:11 +0530

    url

--- client/src/services/httpClient.js
+++ client/src/services/httpClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import config from "../config.json";
 
 const httpClient = axios.create({
-    baseURL: "http://localhost:3000", // Replace with your backend URL
+    baseURL: config?.URL, // Replace with your backend URL
     headers: {
         "Content-Type": "application/json",
     },
